75bc89769b13684b697a11afbac93b44b34fe1ab
Christian Smith <christian.46.smith@gmail.com>
2022-09-06 19:54:24 -0400

    Add functionality

--- index.js
+++ index.js
@@ -1,6 +1,50 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 
+
+const startReadme = (response) => {
+    const data = 
+`# ${response.title} ![license](https://img.shields.io/badge/license-${response.license}-green)
+
+## Description
+${response.description}
+
+## Table of Contents
+- [Installation Guide](#installation-guide)
+- [Usage Information](#usage-information)
+- [License](#license)
+- [Contribution Guidelines](#contribution-guidelines)
+- [Test Instructions](#test-instructions)
+- [Questions](#questions)
+
+
+## Installation Guide
+${response.installation}
+
+## Usage Information
+${response.usage}
+
+## License
+This application is covered under the ${response.license} license.
+
+## Contribution Guidelines
+${response.contribution}
+
+## Test Instructions
+${response.test}
+
+## Questions
+Find my github at [http://github.com/${response.github}](http://github.com/${response.github} "http://github.com/${response.github}")
+
+Contact me by email: ${response.email}`
+
+    if (!fs.existsSync('./output')){
+        fs.mkdirSync('./output');
+    }
+    fs.writeFileSync('./output/README.md', data, (error) => 
+    error ? console.error(error) : console.log("sucess"));
+}
+
 inquirer.prompt([
     {
         type: 'input',
@@ -48,4 +92,4 @@ inquirer.prompt([
         message: 'Email: ',
         name: 'email'
     }
-])
+]).then(startReadme);
